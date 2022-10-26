5d1b2d7b9d3983985c5104fd028790173af848b1
lazaroconceicao <joselazaroc@gmail.com>
2022-10-25 23:15:59 -0300

    filtering events

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -4,22 +4,7 @@ import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: [
-    {
-      descricao: "Estudar React",
-      inicio: new Date("2022-10-24T01:00"),
-      fim: new Date("2022-10-24T05:00"),
-      completo: false,
-      id: 1642342744
-    },
-    {
-      descricao: "Estudar Recoil",
-      inicio: new Date("2022-10-24T07:00"),
-      fim: new Date("2022-10-24T10:00"),
-      completo: false,
-      id: 1642342955
-    },
-  ]
+  default: []
 })
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
